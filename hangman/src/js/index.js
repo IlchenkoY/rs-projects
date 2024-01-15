683a75fc9900b63eef62ba8bd16b7923eead7305
Yaroslav Ilchencko <ilchenckoyaroslav@gmail.com>
2024-01-15 20:54:28 +0200

    feat: fihish base logic, rules 1-9

--- hangman/src/js/index.js
+++ hangman/src/js/index.js
@@ -7,13 +7,18 @@ let currentLetterIndexes;
 let secretWord;
 let incorrectGuesses = 0;
 let quizList;
+let keyList;
 let secretWordLength = 0;
 let guessedLettersLength = 0;
+let selectedButtons = [];
+let gallowImg;
+let randomQuestionId;
 
 function initGame() {
   const { body } = document;
   const container = `<div class="container">${gallow}${quiz}${modal}</div>`;
   body.innerHTML += container;
+  document.addEventListener("keydown", handKeyboard);
 
   handleHint();
   createQuizAnswer();
@@ -22,13 +27,48 @@ function initGame() {
 }
 
 function createVirtualKeyboard() {
+  const alphabet = Array.from({ length: 26 }, (_, index) =>
+    String.fromCharCode("A".charCodeAt(0) + index)
+  );
+  const newKeysLis = alphabet
+    .map(
+      (el) =>
+        `<li class="quiz__letter-item"><button class="quiz__letter">${el}</button></li>`
+    )
+    .join("");
   const keybord = document.querySelector(".quiz__letter-list");
+  keybord.innerHTML = newKeysLis;
+  keyList = Array.from(keybord.children)
+    .map(({ children }) => Array.from(children))
+    .flatMap((el) => el);
   keybord.addEventListener("click", handVirtualKeyboard);
 }
 
 function handVirtualKeyboard(e) {
+  if (e.target.nodeName !== "BUTTON") {
+    return;
+  }
   const letterGuess = e.target.textContent;
-  const currentBtn = e.target;
+  handleLetter(letterGuess);
+}
+
+function handKeyboard({ code }) {
+  if (code.slice(0, 3) !== "Key") {
+    return;
+  }
+  const letterGuess = code.slice(-1);
+  handleLetter(letterGuess);
+}
+
+function handleLetter(letterGuess) {
+  if (selectedButtons.includes(letterGuess)) {
+    return;
+  }
+  selectedButtons.push(letterGuess);
+
+  const currentBtn = Array.from(keyList).filter(
+    (el) => el.textContent === letterGuess
+  )[0];
   currentBtn.disabled = true;
   currentBtn.style.boxShadow = "none";
   currentBtn.style.cursor = "default";
@@ -53,6 +93,7 @@ function handVirtualKeyboard(e) {
 }
 
 function handleQuizAnswer(indexesArr, letter) {
+  gallowImg = document.querySelector(".gallow__img");
   const currentGuessedEl = Array.from(quizList.children).filter((el) =>
     indexesArr.includes(Number(el.dataset.index))
   );
@@ -67,6 +108,7 @@ function handleQuizAnswer(indexesArr, letter) {
     return;
   }
   incorrectGuesses += 1;
+  gallowImg.src = `src/assets/Component ${incorrectGuesses}.png`;
 }
 
 function createQuizAnswer() {
@@ -80,11 +122,16 @@ function createQuizAnswer() {
 
 function handleHint() {
   const question = document.querySelector(".quiz__question");
-  const randomQuestionId = Math.floor(Math.random() * 20) + 1;
-  const randomQuestion = questions.filter(
-    (el) => el.id === randomQuestionId
-  )[0];
+  function randomId() {
+    return randomQuestionId === Math.floor(Math.random() * 20) + 1
+      ? randomId()
+      : Math.floor(Math.random() * 20) + 1;
+  }
+  randomQuestionId = randomId();
 
+  const randomQuestion = questions.filter(
+    ({ id }) => id === randomQuestionId
+  )[0];
   question.textContent = randomQuestion.question;
   secretWord = randomQuestion.answer;
   secretWordLength = secretWord.length;
@@ -96,19 +143,46 @@ function handleGuesses() {
 }
 
 function checkGameStatus() {
-  const modalBackdrop = document.querySelector(".modal-backdrop");
-
-  if (incorrectGuesses >= 6 || secretWordLength === guessedLettersLength) {
-    modalBackdrop.style.display = "block";
+  if (incorrectGuesses >= 6) {
+    displayEndGameModal("lose");
+    return;
   }
-
-  // displayEndGameModal();
+  if (secretWordLength === guessedLettersLength) {
+    displayEndGameModal("win");
+  }
 }
 
-// function displayEndGameModal() {
-// }
+function displayEndGameModal(userStatus) {
+  const modalBackdrop = document.querySelector(".modal-backdrop");
+  const modalTitle = document.querySelector(".modal__title");
+  const modalWord = document.querySelector(".modal-word");
 
-// function resetGame() {
-// }
+  if (userStatus === "win") {
+    modalTitle.textContent = "Win!";
+  }
+  if (userStatus === "lose") {
+    modalTitle.textContent = "Game Ower!";
+  }
+  modalWord.textContent = `${secretWord.toUpperCase()}`;
+  modalBackdrop.style.display = "block";
+  document.addEventListener("click", resetGame);
+}
+
+function resetGame({ target }) {
+  const modalBtn = document.querySelector(".modal-btn");
+  const modalBackdrop = document.querySelector(".modal-backdrop");
+  if (target === modalBtn) {
+    gallowImg.src = `src/assets/gallows.png`;
+    handleHint();
+    createQuizAnswer();
+    incorrectGuesses = 0;
+    guessedLettersLength = 0;
+    handleGuesses();
+    createVirtualKeyboard();
+    selectedButtons = [];
+    modalBackdrop.style.display = "none";
+    document.removeEventListener("click", resetGame);
+  }
+}
 
 window.onload = initGame;
