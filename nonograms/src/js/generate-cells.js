3deb9bd17ff189b6e367bf22daa38e11dd5b5404
Yaroslav Ilchencko <ilchenckoyaroslav@gmail.com>
2024-02-01 23:36:21 +0200

    refactor: change structure of project, delete: game-settings-markup,generate-cells,generate-grid,generate-row-list,generate-row,pictures-grid

--- nonograms/src/js/generate-cells.js
+++
@@ -1,7 +0,0 @@
-export default function generateCells(num = "", size, mod = "") {
-  let cells = "";
-  for (let i = 0; i < size; i += 1) {
-    cells += `<div class="${mod}cell">${num}</div>`;
-  }
-  return cells;
-}
--- nonograms/src/js/generate-row.js
+++
@@ -1,3 +0,0 @@
-export default function generateRow(cells, directionMod = "row") {
-  return `<div class="${directionMod}">${cells}</div>`;
-}
